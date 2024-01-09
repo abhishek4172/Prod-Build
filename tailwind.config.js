4a06dc983bfdb759665716384c77cbed596063f4
Abhishek Mithaigar <abhishek.glb4172@gmail.com>
2024-01-09 08:48:39 +0530

    Git New Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./build/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 50s linear infinite"
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: 'translateX(0)',
+          },
+          to: {
+            transform: 'translateX(calc(-100% - 2.5rem))',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
